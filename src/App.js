5141fcae443aec7ef28aeb25963521d2171a11df
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-07-09 21:59:57 -0400

    fixed error in events

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>This meetup app should render</h1>
         <CitySearch />
         <NumberOfEvents />
         <EventList />
