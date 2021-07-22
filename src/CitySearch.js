1e8c9da4848428692a6d9d3e1a1555cb28ae6fc9
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-07-22 13:31:17 -0400

    added error to inputs
    included errors to the input fields when inputs are wrong

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   
   state = {
     query:'',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: false,
+    infoText:''
   }
   
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({ 
-      query: value,
-      suggestions, 
-    });
+    if (suggestions.length === 0){
+      this.setState({
+        query: value,
+        infoText: `There are no matches for ${value}. Try again.`
+      });
+    } else {
+      this.setState({ 
+        query: value,
+        suggestions,
+        infoText:'' 
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
@@ -31,25 +41,27 @@ class CitySearch extends Component {
   render() { 
     return (
       <div className="CitySearch">
-      <p>Search your city</p>
+        
+        <p>Search your city</p>
         <input type="text" 
           className="city" 
           value={this.state.query} 
           onChange={this.handleInputChanged}
           onFocus={() => { this.setState({ showSuggestions: true }) }} />
-        
+          
         <div>
-        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
-          {this.state.suggestions.map((suggestion) => (
-          <li
-            key={suggestion}
-            onClick={() => this.handleItemClicked(suggestion)}
-            >{suggestion}</li>
-          ))}
-          <li onClick={() => this.handleItemClicked("all")}>
-            <b>See all cities</b>
-          </li>
-        </ul>
+          <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+            {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+              >{suggestion}</li>
+            ))}
+            <li onClick={() => this.handleItemClicked("all")}>
+              <b><InfoAlert text={this.state.infoText} /></b>
+              <b>See all cities</b>
+            </li>
+          </ul>
         </div>
       </div>
     );
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   
   state = {
     numberOfEvents: 12,
+    errorText: ''
   }
   
   handleInputChanged = (event) => {
     const number = event.target.value;
+    if (number <= 0 || number > 99) {
+      return this.setState({
+        errorText: 'Please enter a number between 0 and 99',
+        numberOfEvents:''
+      });
+    } else {
     this.setState({ 
       numberOfEvents: number,
+      errorText: '',
     });
     this.props.updateEventCount(event.target.value);
+    }
   };
 
   render() {
     return (
       <div>
-        <p>Limit search results </p><input type="number" id="numberInput" value={this.state.numberOfEvents} className="numberInput" onChange={(e) => this.handleInputChanged(e)} />
+        <p>Limit search results </p>
+        
+        <input type="number" id="numberInput" value={this.state.numberOfEvents} className="numberInput" onChange={(e) => this.handleInputChanged(e)} />
+        <div className="errorText"><ErrorAlert text={this.state.errorText} /></div>
       </div>
     );
   }
